cd71903f40159ea3ff0efa898fd4b4a89baf7e35
godseun_mac <alcy1024@gmail.com>
2022-10-02 19:41:18 +0900

    disable react strictmode

--- src/index.tsx
+++ src/index.tsx
@@ -13,14 +13,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <AppRoutes />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <AppRoutes />
+  </Provider>
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
